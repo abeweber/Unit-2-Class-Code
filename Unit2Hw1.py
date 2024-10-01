aeaf37c18a4c6a4302b6fadb901dea0014a1d959
abeweber <webera@wlhs.wlwv.k12.or.us>
2024-10-01 08:42:28 -0700

    homework

---
+++ Unit2Hw1.py
@@ -0,0 +1,48 @@
+'''
+Name: Abe Weber
+Date: 9/30/24
+Description: Unit 2 Homework 1
+'''
+
+
+print('Problem 1'.center(20,'-'))
+
+print("card game")
+print("pirate game")
+print("animals game")
+
+
+print('Problem 2'.center(20,'-'))
+
+print( """  _____
+         |A .  | _____
+         | /.\ ||A ^  | _____
+         |(_._)|| / \ ||A _  | _____
+         |  |  || \ / || ( ) ||A_ _ |
+         |____V||  .  ||(_'_)||( v )|
+                |____V||  |  || \ / |
+                       |____V||  .  |
+                              |____V|""")
+
+
+
+
+
+
+Distance = 173.8
+Mpg = int(input("how many miles per gallon does your car get?"))
+Gasprice = int(input("How much is gas in your area?"))
+Gastank = int(input("How many gallons of gas does your car hold?"))
+Distance2 = 173.8//Mpg
+FinalCost = Distance2*Gasprice
+
+print(f"It will cost {FinalCost} dollars to drive from Portland to Seattle")
+
+
+
+
+
+
+
+
+

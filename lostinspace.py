999e0594cdc650f79d752f3379bfef30f2d7154b
abeweber <webera@wlhs.wlwv.k12.or.us>
2024-10-03 09:17:26 -0700

    homework unit2hw1

---
+++ lostinspace.py
@@ -0,0 +1,24 @@
+'''
+Name: Abe Weber
+Date: 10/3/2024
+Description: This is a text adventure game called Lost in Space about someone who crash lands on a planet
+and tries to find their way around the planet
+'''
+# raw text needed for ascii art, use r in front of '''
+intro_art = r"""                     `. ___
+                    __,' __`.                _..----....____
+        __...--.'``;.   ,.   ;``--..__     .'    ,-._    _.-'
+  _..-''-------'   `'   `'   `'     O ``-''._   (,;') _,'
+,'________________                          \`-._`-','
+ `._              ```````````------...___   '-.._'-:
+    ```--.._      ,.                     ````--...__\-.
+            `.--. `-`                       ____    |  |`
+              `. `.                       ,'`````.  ;  ;`
+                `._`.        __________   `.      \'__/`
+                   `-:._____/______/___/____`.     \  `
+                               |       `._    `.    \
+                               `._________`-.   `.   `.___
+                                            SSt  `------'`"""
+print("Welcome to Lost in Space.....")
+#could I possibly
+print(intro_art)

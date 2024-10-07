e7126978443b94f984fd32f4a1f6a6ea95efd4d9
Abe Weber <webera@wlhs.wlwv.k12.or.us>
2024-10-07 16:01:23 +0000

    done

--- madlibfinal.py
+++ madlibfinal.py
@@ -1,3 +1,5 @@
+name = input("What is your name?")
+date = input("What is the date?")
 
 noun = input("Give me a person, place, or thing:")
 noun2 = input("Give me a person, place, or thing:")
@@ -16,15 +18,15 @@ noun6 = input("Give me a person, place, or thing:")
 adverb1 = input("Give me an adverb:")
 noun7 = input("Give me a person, place, or thing:")
 verb6 = input("Give me a verb:")
-noun8 = input("Give me a person, place, or thing:")
 adverb2 = input("Give me an adverb:")
 
-print("Abe Weber")
-print("10/6/2024")
+print (f'Your name is {name.title()}')
+
+print(f"Today's date is {date}")
 
 print(f'''This weekend I am going camping with {noun} . I packed my lantern, sleeping bag, and
-{noun2}. I am so {adjective} to {verb} in a tent. I am {verb2} we
-might see a {noun3}, they are kind of {adjective2}. We are going to hike, fish, and {verb3}.
+{noun2}. I am so {adjective} from {verb} in a tent. I am {verb2}, we
+might see a {noun3}, they are kind of {adjective2}. We are going hiking, fishing, and {verb3}.
 I have heard that the {noun4} lake is great for {verb4}. Then we will be {verb5} through the forest for {noun5}. If I see a
-{adjective3} {noun6} while hiking, I am going to bring it home as a pet! At night we will tell
-{adverb1} {noun7} stories and {verb6} {noun8} around the campfire {adverb2} night!! ''')
+{adjective3} {noun6} while hiking, I am going to bring it home as a pet! At night we will {adverb1} tell
+ {noun7} stories and {verb6} {adverb2} around the campfire all night!! ''')
